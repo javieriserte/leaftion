4004d2d547a0d2d68951c4202b880a8b2ff8212f
Javier <jiserte@unq.edu.ar>
2016-07-02 11:43:46 -0300

    first commit

---
+++ Jtrip/src/org/jiserte/jtrip/CondExamples.java
@@ -0,0 +1,29 @@
+package org.jiserte.jtrip;
+
+/**
+ * Esta clase es para probar el cálculo del número condicional de una matriz 
+ * usando el paquete JAMA
+ * @author Javier
+ *
+ */
+
+import Jama.*; 
+
+public class CondExamples {
+
+  public static void main(String[] args) {
+
+    double[][] m = new double[2][2];
+    
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 3;
+    m[1][1] = 4;
+    
+    Matrix mat = new Matrix(m);
+    
+    System.out.println( mat.cond() );
+    
+  }
+
+}
