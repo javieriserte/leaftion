1f62c31fc7fd701e16c1cf4b2415efaf9da2f947
Javier Iserte <jiserte@leloir.org.ar>
2016-07-06 18:45:48 -0300

    Added JFileChooser to select image folder

--- Leaftion/src/org/jiserte/leaftion/Leaftion.java
+++ Leaftion/src/org/jiserte/leaftion/Leaftion.java
@@ -3,12 +3,16 @@ package org.jiserte.leaftion;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FileDialog;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.List;
@@ -16,6 +20,7 @@ import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -27,152 +32,168 @@ import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 import javax.swing.plaf.basic.BasicBorders;
 
+import org.jiserte.leaftion.image.ImageLibrary;
 import org.jiserte.leaftion.image.ImageLoader;
 import org.jiserte.leaftion.math.MotionEstimator;
 import org.jiserte.leaftion.math.Motions;
 import org.jiserte.multiselectimagepanel.MultiSelectImagePanel;
 
-public class Leaftion extends JFrame{
+public class Leaftion extends JFrame {
 
-  public static void main(String[] args) {
-	  
-	  
-	    try {
-	    	UIManager.setLookAndFeel(
-	    			UIManager.getSystemLookAndFeelClassName());
-	    	
-	    	SwingUtilities.invokeLater(new Runnable() {
-		        public void run() {
-		         
-		          Leaftion pt = new Leaftion();
+	private File imageFolder = null;
+	
+	private ImageLibrary library;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1917154020995579885L;
 
-		          // creates the main instance
-		          
-		          
-		          pt.setVisible(true);
-		          pt.setPreferredSize(new Dimension(1024,768));
-		          pt.setSize(new Dimension(1024,768));
-		          pt.setLocationRelativeTo(null);
-		          pt.setTitle("Leaftion");
-		          pt.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		          
-		          pt.createGUI();
-		          
-		          pt.pack();
-		          
-		          
-		        }
-		      });
-	    	
-	    	
-	    	} catch (Exception e){
-	    		System.out.println(e);
-	    	}
-	    
-	    
-	    
-	  
-//	  
-//
-//    File folder = new File("C:\\Javier\\TRiP-master\\input\\crop_plant1");
-//    
-//    File outfile = new File("C:\\Javier\\TRiP-master\\input\\my_plat.csv");
-//    
-//    try {
-//      
-//      List<BufferedImage> images = (new ImageLoader()).loadFromFolder(folder);
-//      
-//      MotionEstimator mest = new MotionEstimator();
-//      
-//      Motions motions = mest.estimateMotionInSeries(images);
-//      
-//      PrintStream out = new PrintStream(outfile);
-//      
-//      for ( int i =0 ; i< motions.getV_motion().length ; i++ ) {
-//        
-//        out.println( String.format(new Locale("es", "ar"), "%6.4f;%6.4f ",motions.getV_motion()[i] ,motions.getH_motion()[i])) ;
-//        
-//      }
-//      
-//      out.close();
-//      
-//      
-//    
-//    } catch (IOException e) {
-//      // TODO Auto-generated catch block
-//      e.printStackTrace();
-//    }
-    
-  }
+	public static void main(String[] args) {
 
-protected void createGUI() {
-	
-	JLabel textField = new JLabel("Ninguna carpeta elegida");
-	
-//	textField.setBorder(BorderFactory.createBevelBorder(1));
-	
-	textField.setPreferredSize( new Dimension(350,50) );
-	
-	JButton button = new JButton("Buscar Carpeta");
-	
-	button.setToolTipText("Define la carpeta con las imágenes");
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-	JButton buttonBright = new JButton("Brillante");
-	
-	JButton preButton = new JButton("<<");
-	
-	JButton nextButton = new JButton(">>");
-	
-	JLabel counterLabel = new JLabel("0");
-	
-	
-//	counterLabel.setBorder(BorderFactory.createBevelBorder(1));
-	
-	counterLabel.setPreferredSize( new Dimension(50,50) );
-	
-	JButton runButton = new JButton("Run!");
-	
-	JToolBar tb  = new JToolBar();
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
 
-	tb.add(button);
+					Leaftion pt = new Leaftion();
 
-	tb.addSeparator();
+					// creates the main instance
 
-	tb.add(textField);
-	
-	tb.addSeparator();
-	
-	tb.add(buttonBright);
-	
-	tb.addSeparator();
+					pt.setVisible(true);
+					pt.setPreferredSize(new Dimension(1024, 768));
+					pt.setSize(new Dimension(1024, 768));
+					pt.setLocationRelativeTo(null);
+					pt.setTitle("Leaftion");
+					pt.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-	tb.add(preButton);
-	
-	
-	tb.add(nextButton);
+					pt.createGUI();
 
-	tb.addSeparator();
-	
-	tb.add(counterLabel);
-	
-	tb.addSeparator();
+					pt.pack();
 
-	tb.add(runButton);
+				}
 
-	
-	tb.setRollover(true);
-	
-	tb.setFloatable(false);
-	
-	tb.setPreferredSize(new Dimension(100,50)); 
+			});
 
-	
-	this.add(tb,BorderLayout.PAGE_START);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
+		//
+		//
+		// File folder = new
+		// File("C:\\Javier\\TRiP-master\\input\\crop_plant1");
+		//
+		// File outfile = new
+		// File("C:\\Javier\\TRiP-master\\input\\my_plat.csv");
+		//
+		// try {
+		//
+		// List<BufferedImage> images = (new
+		// ImageLoader()).loadFromFolder(folder);
+		//
+		// MotionEstimator mest = new MotionEstimator();
+		//
+		// Motions motions = mest.estimateMotionInSeries(images);
+		//
+		// PrintStream out = new PrintStream(outfile);
+		//
+		// for ( int i =0 ; i< motions.getV_motion().length ; i++ ) {
+		//
+		// out.println( String.format(new Locale("es", "ar"), "%6.4f;%6.4f
+		// ",motions.getV_motion()[i] ,motions.getH_motion()[i])) ;
+		//
+		// }
+		//
+		// out.close();
+		//
+		//
+		//
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+	}
+
+	protected void createGUI() {
+
+		JLabel textField = new JLabel("Ninguna carpeta elegida");
+
+		textField.setPreferredSize(new Dimension(350, 50));
+
+		JButton button = new JButton("Buscar Carpeta");
 		
-	this.add(new MultiSelectImagePanel(), BorderLayout.CENTER);
+		button.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				JFileChooser fc = new JFileChooser();
+				
+				fc.setMultiSelectionEnabled(false);
+				
+				fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				
+				fc.showOpenDialog(Leaftion.this);
+				
+				File a = fc.getSelectedFile(); 
+				
+		
+			}
+		} );
 
-	
-}
+		button.setToolTipText("Define la carpeta con las imágenes");
+
+		JButton buttonBright = new JButton("Brillante");
+
+		JButton preButton = new JButton("<<");
+
+		JButton nextButton = new JButton(">>");
+
+		JLabel counterLabel = new JLabel("0");
+
+		counterLabel.setPreferredSize(new Dimension(50, 50));
+
+		JButton runButton = new JButton("Run!");
+
+		JToolBar tb = new JToolBar();
+
+		tb.add(button);
+
+		tb.addSeparator();
+
+		tb.add(textField);
+
+		tb.addSeparator();
+
+		tb.add(buttonBright);
+
+		tb.addSeparator();
+
+		tb.add(preButton);
+
+		tb.add(nextButton);
+
+		tb.addSeparator();
+
+		tb.add(counterLabel);
+
+		tb.addSeparator();
+
+		tb.add(runButton);
+
+		tb.setRollover(true);
+
+		tb.setFloatable(false);
+
+		tb.setPreferredSize(new Dimension(100, 35));
+
+		this.add(tb, BorderLayout.PAGE_START);
+
+		this.add(new MultiSelectImagePanel(), BorderLayout.CENTER);
+
+	}
 
 }
--- Leaftion/src/org/jiserte/leaftion/image/ImageLibrary.java
+++ Leaftion/src/org/jiserte/leaftion/image/ImageLibrary.java
@@ -1,167 +1,208 @@
 package org.jiserte.leaftion.image;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
 public class ImageLibrary {
 
-  private File[] imageFiles;
-  
-  private BufferedImage currentImage;
-  
-  private int currentImageIndex = 0;
-  
-  private int lastRetrievedImageIndex = -1;
-  
-  private BufferedImage brightCompositeImage;
+	private File[] imageFiles;
 
-  
-  public void getImageFilesInFolder(File folder) {
-    
-    this.setImageFiles(folder.listFiles(new JpegFileNameFilter()) );
-    
-  }
-  
-  public BufferedImage next() {
-    
-      this.currentImageIndex ++;
-    
-    return this.getCurrentImage();
-    
-  }
-  
-  public File[] getImageFiles() {
-    return imageFiles;
-  }
+	private BufferedImage currentImage;
 
+	private int currentImageIndex = 0;
 
-  private void setImageFiles(File[] imageFiles) {
-    this.imageFiles = imageFiles;
-  }
-  
-  public void exportBright(File outfile) {
-    try {
-      ImageIO.write(this.brightCompositeImage, "jpeg", outfile);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
+	private int lastRetrievedImageIndex = -1;
 
+	private BufferedImage brightCompositeImage;
 
-  public BufferedImage getCurrentImage() {
-    
-    if ( this.currentImageIndex < 0 ) {
-      this.currentImageIndex = 0;
-   }
-    
-    if (this.currentImageIndex >= this.getImageFiles().length) {
-      this.currentImageIndex = this.getImageFiles().length -1;
-   }
-    
-    if (this.lastRetrievedImageIndex != this.currentImageIndex) {
-      this.setCurrentImage();
-   }
-    
-    return currentImage;
-    
-  }
-  
-  public void setCurrentImage() {
-  
-    try {
-      this.currentImage = ImageIO.read(this.getImageFiles()[this.currentImageIndex]);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    this.lastRetrievedImageIndex = this.currentImageIndex;
-    
-    
-  }
-  
-  
-  public void makeBrightComposite() {
-    
-    if ( this.currentImage == null) {
-      this.setCurrentImage();
-    }
-    
-    int width = this.currentImage.getWidth();
-    int height = this.currentImage.getHeight();
-    double [][] colapse = new double[width][height];
-    
-    ImageProcessor ip = new ImageProcessor();
-    
-    
-    int imageCounter = 0;
-    
-    for (File file : this.getImageFiles()) {
-      
-      try {
-        
-        imageCounter ++;
-        
-        System.out.println("Current Image Number : " + imageCounter);
-        
-        
-        BufferedImage cImage = ImageIO.read(file);
-        
-        double[][] cImageInt = ip.toGrayScaleArray(cImage);
-        
-        for (int x = 0; x < width ; x++) {
-          
-          for (int y = 0; y < height ; y++) {
-            
-            colapse[x][y] = Math.max(colapse[x][y], cImageInt[x][y]);
-            
-          }
-          
-        }
-        
-        
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-      
-    }
+	public void getImageFilesInFolder(File folder) {
 
-    
-    BufferedImage bright = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    
-    for (int x = 0; x < width ; x++) {
-      
-      for (int y = 0; y < height ; y++) {
-        
-        float br = (float) colapse[x][y] / 255; 
-        
-        bright.setRGB(x, y, new Color(br, br, br).getRGB());
-        
-      }
-      
-    }
-    
-    this.brightCompositeImage = bright;
-    
-  }
-  
-  public static void main(String[] args) {
-    
-    ImageLibrary il = new ImageLibrary();
-    
-    il.getImageFilesInFolder(new File("c:\\javier\\TRiP-master\\input"));
-    
-    il.makeBrightComposite();
-    
-    il.exportBright(new File("c:\\javier\\TRiP-master\\bright.jpg"));
-    
-  }
+		this.setImageFiles(folder.listFiles(new JpegFileNameFilter()));
 
-  
+	}
+
+	public BufferedImage next() {
+
+		this.currentImageIndex++;
+
+		return this.getCurrentImage();
+
+	}
+
+	public File[] getImageFiles() {
+		return imageFiles;
+	}
+
+	private void setImageFiles(File[] imageFiles) {
+		this.imageFiles = imageFiles;
+	}
+
+	public void exportBright(File outfile) {
+		try {
+			ImageIO.write(this.brightCompositeImage, "jpeg", outfile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public BufferedImage getCurrentImage() {
+
+		if (this.currentImageIndex < 0) {
+			this.currentImageIndex = 0;
+		}
+
+		if (this.currentImageIndex >= this.getImageFiles().length) {
+			this.currentImageIndex = this.getImageFiles().length - 1;
+		}
+
+		if (this.lastRetrievedImageIndex != this.currentImageIndex) {
+			this.setCurrentImage();
+		}
+
+		return currentImage;
+
+	}
+
+	public void setCurrentImage() {
+
+		try {
+			this.currentImage = ImageIO.read(this.getImageFiles()[this.currentImageIndex]);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.lastRetrievedImageIndex = this.currentImageIndex;
+
+	}
+
+	public void makeBrightComposite() {
+
+		if (this.currentImage == null) {
+			this.setCurrentImage();
+		}
+
+		int width = this.currentImage.getWidth();
+		int height = this.currentImage.getHeight();
+		double[][] colapse = new double[width][height];
+
+		ImageProcessor ip = new ImageProcessor();
+
+		int imageCounter = 0;
+
+		for (File file : this.getImageFiles()) {
+
+			try {
+
+				imageCounter++;
+
+				System.out.println("Current Image Number : " + imageCounter);
+
+				BufferedImage cImage = ImageIO.read(file);
+
+				double[][] cImageInt = ip.toGrayScaleArray(cImage);
+
+				for (int x = 0; x < width; x++) {
+
+					for (int y = 0; y < height; y++) {
+
+						colapse[x][y] = Math.max(colapse[x][y], cImageInt[x][y]);
+
+					}
+
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		BufferedImage bright = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+		for (int x = 0; x < width; x++) {
+
+			for (int y = 0; y < height; y++) {
+
+				float br = (float) colapse[x][y] / 255;
+
+				bright.setRGB(x, y, new Color(br, br, br).getRGB());
+
+			}
+
+		}
+
+		this.brightCompositeImage = bright;
+
+	}
+
+	public List<List<BufferedImage>> cropRegions(List<Rectangle> regions) {
+
+		List<List<BufferedImage>> result = new ArrayList<>();
+		
+		for (int i =0 ; i< regions.size(); i++) {
+			result.add(new ArrayList<>());
+		}
+		
+		for (File file : this.imageFiles) {
+			
+			int index = 0;
+
+			try {
+
+				BufferedImage ci = ImageIO.read(file);
+
+				for (Rectangle region : regions) {
+					
+					BufferedImage im = new BufferedImage(
+							(int)region.getWidth(), 
+							(int)region.getHeight(), 
+							BufferedImage.TYPE_INT_RGB) ;
+					
+					Graphics2D g = (Graphics2D) im.getGraphics();
+					
+					g.drawImage(ci,
+							0, 0,
+							(int)region.getWidth(),(int)region.getHeight(),
+							(int)region.getX(), (int)region.getY(), 
+							(int)(region.getX() + region.getWidth()) ,
+							(int)(region.getY() + region.getHeight()) , null);
+					
+					result.get(index).add(im);
+					
+					index++;
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return result;
+
+	}
+
+	public static void main(String[] args) {
+
+		ImageLibrary il = new ImageLibrary();
+
+		il.getImageFilesInFolder(new File("c:\\javier\\TRiP-master\\input"));
+
+		il.makeBrightComposite();
+
+		il.exportBright(new File("c:\\javier\\TRiP-master\\bright.jpg"));
+
+	}
+
 }
